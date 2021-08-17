d2fb345cb18c4af449751e464dd20ea76cfa2f4d
greyhenna <72964976+greyhenna@users.noreply.github.com>
2021-08-17 21:38:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   Admins: ["UserID", "UserID"], //Admins of the bot
-  ExpressServer: true,//If you wanted to make the website run or not
+  ExpressServer: false,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/EaDSxW6Tj6", //Support Server Link
