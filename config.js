9441d3aa462e53e598f7adc441b901bb9600ac6f
greyhenna <72964976+greyhenna@users.noreply.github.com>
2021-08-21 18:24:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   Admins: ["UserID", "UserID"], //Admins of the bot
-  ExpressServer: false,//If you wanted to make the website run or not
+  ExpressServer: true,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
-  Port: 3000, //Which port website gonna be hosted
+  Port: 443, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/EaDSxW6Tj6", //Support Server Link
   Token: process.env.Token || "", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
@@ -14,7 +14,7 @@ module.exports = {
   IconURL:
     "https://i.ibb.co/Fm6Gnkw/A-a3f95bcff202159f64ed352f3b505e53.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
-  Website: process.env.Website || "0.0.0.0", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+  Website: process.env.Website || "https://musicaug15.herokuapp.com/", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
 
   //Lavalink
    Lavalink: {
